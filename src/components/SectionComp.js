ff2082e8bc8af512e386d8852d873fa304eb01da
Sebas <sebastian.ituarte@gmail.com>
2020-07-21 19:57:39 -0400

    Ajuste en tamaño de imagenes
    Ajuste en tamaño de imagenes

--- src/components/SectionComp.js
+++ src/components/SectionComp.js
@@ -8,7 +8,7 @@ const SectionComp = props => {
             <Jumbotron>
                 <Container className="w-100">
                     <Row className="justify-content-center align-items-center">
-                        <Col xs={12} sm={12} md={12} lg={10} xl={8}>
+                        <Col xs={12} sm={12} md={10} lg={8} xl={6}>
                             <img className="align-self-center rounded w-100" src={props.img} alt={props.title} />
                         </Col>
                         <Col xs={12} sm={12} md={12} lg={10} xl={8}  className="mt-5">
